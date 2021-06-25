88667761279edd87cd7920808ef262170e669cc9
Kareem Mohammed Talaat <kareemamr213@aucegypt.edu>
2021-06-25 19:57:14 +0200

    Kareem's version 1.0

--- Disassembler/main.cpp
+++ Disassembler/main.cpp
@@ -1,30 +1,9 @@
-/*
-	This is just a skeleton. It DOES NOT implement all the requirements.
-	It only recognizes the RV32I "ADD", "SUB" and "ADDI" instructions only.
-	It prints "Unkown Instruction" for all other instructions!
-
-	Usage example:
-		$ rvcdiss t1.bin
-	should print out:
-		0x00000000	0x00100013	ADDI	x0, x0, 0x1
-		0x00000004	0x00100093	ADDI	x1, x0, 0x1
-		0x00000008	0x00100113	ADDI	x2, x0, 0x1
-		0x0000000c	0x001001b3	ADD		x3, x0, x1
-		0x00000010	0x00208233	ADD		x4, x1, x2
-		0x00000014	0x004182b3	ADD		x5, x3, x4
-		0x00000018	0x00100893	ADDI	x11, x0, 0x1
-		0x0000001c	0x00028513	ADDI	xa, x5, 0x0
-		0x00000020	0x00000073	Unkown Instruction
-
-	References:
-	(1) The risc-v ISA Manual ver. 2.1 @ https://riscv.org/specifications/
-	(2) https://github.com/michaeljclark/riscv-meta/blob/master/meta/opcodes
-*/
-#include <bitset>
 #include <iostream>
 #include <fstream>
 #include "stdlib.h"
 #include <iomanip>
+
+using namespace std;
 
 unsigned int pc = 0x0;
 
@@ -32,17 +11,17 @@ char memory[8 * 1024];	// only 8KB of memory located at address 0
 
 void emitError(const char* s)
 {
-	std::cout << s;
+	cout << s;
 	exit(0);
 }
 
 void printPrefix(unsigned int instA, unsigned int instW) {
-	std::cout << "0x" << std::hex << std::setfill('0') << std::setw(8) << instA << "\t0x" << std::setw(8) << instW;
+	cout << "0x" << hex << std::setfill('0') << std::setw(8) << instA << "\t0x" << std::setw(8) << instW;
 }
 
 void instDecExec(unsigned int instWord)
 {
-	unsigned int rd, rs1, rs2, funct3, funct7, opcode;
+	unsigned int rd, rs1, rs2, funct3, funct7, opcode, imm1to10, imm11, imm12to19, imm20, imm0, imm;
 	unsigned int I_imm, S_imm, B_imm, U_imm, J_imm;
 	unsigned int address;
 
@@ -53,36 +32,53 @@ void instDecExec(unsigned int instWord)
 	funct3 = (instWord >> 12) & 0x00000007;
 	rs1 = (instWord >> 15) & 0x0000001F;
 	rs2 = (instWord >> 20) & 0x0000001F;
-
+	funct7 = (instWord >> 25) & 0x0000007F;
 	// — inst[31] — inst[30:25] inst[24:21] inst[20]
-	I_imm = ((instWord >> 20) & 0x7FF) | (((instWord >> 31) ? 0xFFFFF800 : 0x0));
+	J_imm = (((instWord >> 12) & 0x000000FF) << 12) | (((instWord >> 20) & 1) << 11) | (((instWord >> 21) & 0x000003FF) << 1) |
+		((instWord >> 31) ? 0xFFFFF800 : 0x0);
 
 	printPrefix(instPC, instWord);
 
-	if (opcode == 0x33) {// R Instructions
-		funct7 = (instWord >> 24) & 0x0000007F;
+	if (opcode == 0x33) {		// R Instructions
 		switch (funct3) {
 		case 0: if (funct7 == 32) {
-			std::cout << "\tSUB\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			cout << "\tSUB\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
 		}
 			  else {
-			std::cout << "\tADD\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			cout << "\tADD\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
 		}
 			  break;
-		default:
-			std::cout << "\tUnkown R Instruction \n";
+		case 1: cout << "\tSLL\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			break;
+		case 2: cout << "\tSLT\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			break;
+		case 3: cout << "\tSLTU\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			break;
+		case 4: cout << "\tXOR\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			break;
+		case 5: if (funct7 == 32) {
+			cout << "\tSRL\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
 		}
-	}
-	else if (opcode == 0x13) {	// I instructions
-		switch (funct3) {
-		case 0:	std::cout << "\tADDI\tx" << rd << ", x" << rs1 << ", " << std::hex << "0x" << (int)I_imm << "\n";
+			  else {
+			cout << "\tSRA\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+		}
+			  break;
+		case 6: cout << "\tOR\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
+			break;
+		case 7: cout << "\tAND\tx" << rd << ", x" << rs1 << ", x" << rs2 << "\n";
 			break;
 		default:
-			std::cout << "\tUnkown I Instruction \n";
+			cout << "\tUnkown R Instruction \n";
 		}
 	}
+
+	else if (opcode == 0x6F)
+	{
+		cout << "\tJAL\tx" << rd << ", " << hex << "0x" << (int)J_imm << "\n";
+	}
 	else {
-		std::cout << "\tUnkown Instruction \n";
+		cout << "\tUnkown J Instruction \n";
+
 	}
 
 }
@@ -90,12 +86,12 @@ void instDecExec(unsigned int instWord)
 int main(int argc, char* argv[]) {
 
 	unsigned int instWord = 0;
-	std::ifstream inFile;
-	std::ofstream outFile;
+	ifstream inFile;
+	ofstream outFile;
 
-	if (argc < 2) emitError("use: rvcdiss <machine_code_file_name>\n");
 
-	inFile.open(argv[1], std::ios::in | std::ios::binary | std::ios::ate);
+
+	inFile.open(argv[1], ios::in | ios::binary | ios::ate);
 
 	if (inFile.is_open())
 	{
@@ -116,22 +112,4 @@ int main(int argc, char* argv[]) {
 		}
 	}
 	else emitError("Cannot access input file\n");
-
-	/*
-		whateverOut = instWord >> 7
-		imm11 = whateverOut & 1
-		whateverOut = whateverOut >> 1
-		imm4to1 = whateverOut & 0b1111;
-		whateverOut = instWord >> 25;
-		imm10to5 = whateverOut & 0b111111;
-		imm12 = whateverOut & 0b1000000;
-
-		imm11 << 11;
-		imm12 << 12
-		imm10to5 << 5;
-		imm4to1 << 1;
-
-	*/
-
-
 }
